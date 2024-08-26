a6ba3db5d59b9e247cc2eeec8402c653146d572a
Roddys-Codex <daniel.kevin.roddy@gmail.com>
2024-08-26 22:54:16 +0100

    Functionality updated to include history so players can view previous moves in the game.

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,14 +2,32 @@ import { Board } from "./index";
 import { useState } from "../utils/index";
 
 export default function Game() {
-    const [xIsNext, setXIsNext] = useState(true);
+
     const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[history.length - 1];
+    const [currentMove, setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove];
+    const xIsNext = currentMove % 2 === 0;
 
     function handlePlay(nextSquares) {
-        setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1)
     }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((squares, move) => {
+
+        let desc = (move > 0) ? "Go to move #" + move : "Go to game start";
+
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+        )
+    });
 
     return (
         <div className="game">
@@ -18,7 +36,7 @@ export default function Game() {
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
             </div>
             <div className="game-info">
-                <ol>{/* TODO */}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
